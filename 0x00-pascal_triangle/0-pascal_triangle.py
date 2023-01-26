c20a26d70610069cae450d2bb053606c25dc0058
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2023-01-26 20:35:00 +0100

    finishing up

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+	triangle = []
+	last = []
+	
+	if n <= 0:
+		return []
+	triangle.append([1])
+	if n == 1:
+		return triangle
+	
+	last = triangle
+		
+	for i in range(n-1):
+		j = 1
+		sub = [1]
+		while j < (len(last)+i)-1:
+			try:
+				sub.append(last[j-1] + last[j])
+			except IndexError:
+				break
+			j += 1
+		sub.append(1)
+		triangle.append(sub)
+		last = sub[:]
+	return triangle
