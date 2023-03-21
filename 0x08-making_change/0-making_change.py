639fca7c05bbc657272a8f0f541bf8834f00cf54
Nosa <nosakhareaghedo42@gmail.com>
2023-03-21 20:25:11 +0000

    trying to pass the check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-"""
-goal: Given a pile of coins of different values,
-determine the fewest number of coins
-needed to meet a given amount total.
-"""
 
 
 def makeChange(coins, total) -> int:
-    """
-    Args: coins(a list of ints)
-    total( an int)
-    objective: determine the fewest number of coins
-    needed to meet a given amount 'total'
-    return: number of coins needed at minimum to equal 'total'
-    """
     if total <= 0:
         return 0
     count: int = 0
@@ -33,12 +21,6 @@ def makeChange(coins, total) -> int:
 
 
 def maximum(coins, waste_coins) -> int:
-    """
-    Args: coins(a list of ints)
-    waste_coins(a list of ints)
-    objective: find the max number that is not in 'waste_coins'
-    return: the max number that is not in waste_coins
-    """
     max_coin: int = coins[0]
 
     for coin in coins:
