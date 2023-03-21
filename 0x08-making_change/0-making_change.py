c7c58b1b4b36ecd5cd58fb62eab204795cf91a2b
Nosa <nosakhareaghedo42@gmail.com>
2023-03-21 20:11:55 +0000

    finishing up

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,8 @@ determine the fewest number of coins
 needed to meet a given amount total.
 """
 
-def makeChange(coins, total):
+
+def makeChange(coins, total) -> int:
     """
     Args: coins(a list of ints)
           total( an int)
@@ -15,31 +16,30 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    count = 0
-    amount = 0
-    waste_coins = []
+    count: int = 0
+    amount: int = 0
+    waste_coins: int = []
 
     while (len(waste_coins) < len(coins)):
         max_coin = maximum(coins, waste_coins)
         if amount == total:
             return count
-        if amount +  max_coin > total:
-             waste_coins.append(max_coin)
+        if amount + max_coin > total:
+            waste_coins.append(max_coin)
         else:
             amount += max_coin
             count += 1
     return -1
 
 
-
-def maximum(coins, waste_coins):
+def maximum(coins, waste_coins) -> int:
     """
      Args: coins(a list of ints)
           waste_coins(a list of ints)
      objective: find the max number that is not in 'waste_coins'
      return: the max number that is not in 'waste_coins'
     """
-    max_coin = coins[0]
+    max_coin: int = coins[0]
 
     for coin in coins:
         if coin > max_coin and coin not in waste_coins:
