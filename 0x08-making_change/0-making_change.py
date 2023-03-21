3b847a0ac88d2bee74e534bcd6d939f022d3ff91
Nosa <nosakhareaghedo42@gmail.com>
2023-03-21 20:37:22 +0000

    trying to pass the check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,16 +5,16 @@ needed to meet a given amount total
 """
 
 
-def makeChange(coins, total) -> int:
+def makeChange(coins, total):
     """
     determines the fewest number of coins
     needed to meet a given amount total
     """
     if total <= 0:
         return 0
-    count: int = 0
-    amount: int = 0
-    waste_coins: int = []
+    count = 0
+    amount = 0
+    waste_coins = []
 
     while (len(waste_coins) < len(coins)):
         max_coin = maximum(coins, waste_coins)
@@ -27,14 +27,15 @@ def makeChange(coins, total) -> int:
             count += 1
     return -1
 
-
-def maximum(coins, waste_coins) -> int:
+"""
+def maximum(coins, waste_coins):
     """
     finds the max coin not in waste_coins
     """
-    max_coin: int = coins[0]
+    max_coin = coins[0]
 
     for coin in coins:
         if coin > max_coin and coin not in waste_coins:
             max_coin = coin
     return max_coin
+    """
