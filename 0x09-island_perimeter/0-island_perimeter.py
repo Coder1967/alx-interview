8bc2e7eddeed1db11c109a2c785b0b5c7c23bcdf
Nosa <nosakhareaghedo42@gmail.com>
2023-03-30 03:49:08 +0000

    trying to pass checker

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""alx question to train for interviews.
+"""Island perimeter computing module.
 """
 
 
 def island_perimeter(grid):
-    """check the readme for task details.
+    """Computes the perimeter of an island with no lakes.
     """
-    if grid is None or len(grid) == 0:
+    perimeter = 0
+    if type(grid) != list:
         return 0
-
-    height: int = 0
-    width: int = 0
-    check = {}
-    check['col'] = -10
-    i = 0
-
-    while i < len(grid):
-        j = 0
-        check['row'] = i
-        while j < len(grid[0]):
-            if grid[i][j] == 1 and check['row'] == i:
-                width += 1
-            if grid[i][j] == 1 and check['col'] == -10:
-                check['col'] = j
-            if grid[i][j] == 1 and check['col'] == j:
-                height += 1
-            j += 1
-        i += 1
-        if width == 1:
-            width = 0
-    return 2 * (width + height)
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
