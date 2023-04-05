08b46d0eb8f2d0cc19ca7cc032e7c43f9397be06
Nosa <nosakhareaghedo42@gmail.com>
2023-04-05 06:13:18 +0000

    refactoring to make it significantly faster

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -37,46 +37,36 @@ def remove_multiples(num, nums):
     return new_list
 
 
+def prime_filter(nums):
+    new_nums = []
+
+    for num in nums:
+        if num == 1:
+            continue
+        if num == 2 or num == 3 or num == 5 or num == 7:
+            new_nums.append(num)
+        elif num % 2 != 0 and num % 3 != 0 and num % 5 != 0 and num % 7 != 0:
+            new_nums.append(num)
+    return new_nums
+
 def isWinner(x, nums) -> str:
     '''
     check the README file for details on what
     problem this function solves.
     '''
+    if x <= 0:
+        return None
     player_wins = {'Maria': 0, 'Ben': 0}
 
-    for i in range(1, (x + 1)):
-        j = 0
-        players = {'Maria': False, 'Ben': False}
-        tracker = 1
-        numbers = num_gen(nums[i - 1])
-        numbers_length = len(numbers)
-        while j < numbers_length:
-            try:
-                if numbers[j] == 1 and tracker % 2 != 0:
-                    players['Maria'] = False
-                    players['Ben'] = True
-                    break
-                elif numbers[j] == 1 and tracker % 2 == 0:
-                    players['Ben'] = False
-                    players['Maria'] = True
-                    break
-                numbers = remove_multiples(numbers[j], numbers)
-                if tracker % 2 != 0:
-                    players['Maria'] = True
-                    players['Ben'] = False
-                elif tracker % 2 == 0:
-                    players['Ben'] = True
-                    players['Maria'] = False
-                j -= numbers_length - len(numbers)
-                numbers_length = len(numbers)
-                tracker += 1
-            except IndexError:
-                pass
-            j = 0
-        if players['Maria'] is True:
-            player_wins['Maria'] += 1
-        elif players['Ben'] is True:
+    for num in nums:
+        numbers = num_gen(num)
+        numbers = prime_filter(numbers)
+
+        if len(numbers) % 2 == 0:
             player_wins['Ben'] += 1
+        elif len(numbers) % 2 != 0:
+            player_wins['Maria'] += 1
+
     if player_wins['Maria'] > player_wins['Ben']:
         return 'Maria'
     elif player_wins['Ben'] > player_wins['Maria']:
